0af295686366e88e6d01e7052a7ee8cf2387d5d7
Tom <thomas.c.almquist@gmail.com>
2019-07-26 22:26:49 -0500

    Built base customer and service models/migrations

--- SubTrack/settings.py
+++ SubTrack/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'services.apps.ServicesConfig',
+    'customers.apps.CustomersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from services.models import Service
 
-# Create your models here.
+class Customer(models.Model):
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	email = models.CharField(max_length=100)
+	services = models.ForeignKey(
+		Service,
+		null=True,
+		blank=True,
+		default=None,
+		on_delete=models.SET_NULL,
+	)
+
+	CONFIRMED = 'co'
+	SET_UP = 'su'
+	ACTIVE = 'ac'
+	CANCELLED = 'ca'
+	ACCOUNT_STATE_CHOICES = [
+		(CONFIRMED, 'confirmed'),
+		(SET_UP, 'set-up'),
+		(ACTIVE, 'active'),
+		(CANCELLED, 'cancelled'),
+	]
+	account_state = models.CharField(
+		max_length=2,
+		choices=ACCOUNT_STATE_CHOICES,
+		default=CONFIRMED,
+	)
+
